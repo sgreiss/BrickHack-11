19561591192ffe5793529131b56967f571c8159d
Ryan Kong <rsk7620@rit.edu>
2025-02-22 16:01:31 -0500

    Created Screw Interactable

---
+++ src/interactables/Screw.java
@@ -0,0 +1,17 @@
+package src.interactables;
+
+import src.Item;
+import src.Item.item_type;
+
+public class Screw extends Interactable {
+    public Screw(String screwname){
+        super(screwname);
+    }
+    public boolean unscrew(Item i){
+        if (i.getType() == item_type.SCREWDRIVER){
+            super.use();
+            return true;
+        }
+        return false;
+    }
+}
