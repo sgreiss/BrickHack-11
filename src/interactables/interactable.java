92672d20f8178efbb642603bae0b7a60d9eb3609
Ryan Kong <rsk7620@rit.edu>
2025-02-22 14:48:04 -0500

    get methods added to interactable

--- src/interactables/interactable.java
+++ src/interactables/interactable.java
@@ -1,16 +1,13 @@
 package src.interactables;
 public abstract class interactable {
     //types: safe, table, paper
-    private boolean usable;
-    private String name;
+    private boolean usable; //whether the object can be interacted with or not
+    private String name; //name of the interactable object
 
     public interactable(String name){
         usable = true;
         this.name = name;
     }
-
-    
-
     public boolean use(){
         if (usable == true){
             usable = false;
@@ -19,5 +16,11 @@ public abstract class interactable {
         else {
             return false;
         }
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean getUsable(){
+        return usable;
     }
 }
