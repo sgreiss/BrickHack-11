c88fa61dc8f4d0265054f7b3718680886e3f114d
Ryan Kong <rsk7620@rit.edu>
2025-02-22 18:49:04 -0500

    Split Lock back into Combo and Key Locks. Added unlockable superclass

--- src/interactables/Screw.java
+++ src/interactables/Screw.java
@@ -3,14 +3,14 @@ package src.Interactables;
 import src.Item;
 import src.Item.item_type;
 
-public class Screw extends Interactable {
+public class Screw extends Unlockable {
     public Screw(String screwname) {
-        super(screwname);
+        super(u_type.SCREW, screwname);
     }
     public boolean unscrew(Item i) {
-        if (i.getType() != item_type.SCREWDRIVER || !getUsable()){
+        if (i.getType() != item_type.SCREWDRIVER || !isUsable()){
             return false;
         }
-        return use();
+        return unlock();
     }
 }
