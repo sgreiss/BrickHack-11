170f325b00d5c51a9f62d6ebbfd1c07429ac53a7
sgreiss <greiss.spencer@gmail.com>
2025-02-22 13:54:02 -0500

    player and escape_room classes more developed

--- escape_room.java
+++ escape_room.java
@@ -1,4 +1,8 @@
 public class escape_room {
+    private static final int NUM_SCREENS = 4;
+    private static final int NUM_ITEMS = 6;
+    private 
+
     public static void main(String[] args) {
         System.out.println("working");
     }
--- player.java
+++ player.java
@@ -1,18 +1,28 @@
+import java.util.*;
+
 public class player {
-    private static ArrayList<item> inventory;
-    private static final HashMap<interactable, String> ALLHINTS = new HashMap();
-    private static ArrayList<String> knownhints;
+    private static item[] inventory;
+    private static final HashMap<interactable, String> ALLHINTS = new HashMap<>(){
+
+    };
+    private static boolean[] known_hints;
 
 
-    public player(){
-        inventory = new ArrayList<>();
-        knownhints = new ArrayList<>();
+    public player() {
+        inventory = new item[6];
+        known_hints = new boolean[ALLHINTS.size()];
     }
 
-    public void addHint(interactable i)){
-        knownhints.add(ALLHINTS.get(i));
-        
+    public void addHint(interactable i) {
+        // known_hints[]
     }
 
-    
+    public void addItem(item i) {
+        for (int j = 0; j < inventory.length; j++) {
+            if (inventory[j] == null) {
+                inventory[j] = i;
+                break;
+            }
+        }
+    }
 }
