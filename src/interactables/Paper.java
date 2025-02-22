d63969dce0ab69bac289ee3ed1912a7812da75e7
Ryan Kong <rsk7620@rit.edu>
2025-02-22 17:31:22 -0500

    Added Paper Interactable

---
+++ src/interactables/Paper.java
@@ -0,0 +1,13 @@
+package src.Interactables;
+
+public class Paper extends Interactable{
+    private String contents;
+
+    public Paper (String name, String contents){
+        super(name);
+        this.contents = contents;
+    }
+    public String getContents(){
+        return contents;
+    }
+}
