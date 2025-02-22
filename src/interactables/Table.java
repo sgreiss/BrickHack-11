d7538c3b9f215c3c16f303139e82560669c6b76f
Ryan Kong <rsk7620@rit.edu>
2025-02-22 17:14:24 -0500

    Added accessor for interactables on table

--- src/interactables/Table.java
+++ src/interactables/Table.java
@@ -10,4 +10,7 @@ public class Table extends Interactable{
     public void placeOnTop(Interactable i) {
         onTop.add(i);
     }
+    public Interactable checkInteractable(int index) {
+        return onTop.get(index);
+    }
 }
