1b2283f2a5a8860508cce42d193fdd21c4011ee9
sgreiss <greiss.spencer@gmail.com>
2025-02-22 13:18:45 -0500

    item.java more developed with get methods and necessary variables

--- item.java
+++ item.java
@@ -5,11 +5,28 @@ public class item {
     private item_type type; // the type of item
     private String name; // the name of the item
     private String description; // the description of the item
-    
+    private String menu; // the menu that pulls up when the item is intereacted with
 
-    public item(item_type type, String name, String description, boolean useable) {
+    public item(item_type type, String name, String description, String menu) {
         this.type = type;
         this.name = name;
         this.description = description;
+        this.menu = menu;
+    }
+
+    public item_type getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMenu() {
+        return menu;
     }
 }
