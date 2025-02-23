cfa3030bc6e6af5285c0336bc1a4b22818a34510
Ryan Kong <rsk7620@rit.edu>
2025-02-23 00:01:09 -0500

    Changes

--- src/gui/Model.java
+++ src/gui/Model.java
@@ -13,7 +13,7 @@ public class Model {
     }
     //Game Messages
     public static final String STARTMSG = "READ THE MESSAGE BELOW AND CLICK BUTTON TO START";
-    public static final String WELCOME = "Welcome to the Escape Room!\n You are trapped in a room and must find a way out.\n Use the items in the room to solve the puzzles and escape.";
+    public static final String WELCOME = "Welcome to the Escape Room!\nYou are trapped in a room and must find a way out.\nUse the items in the room to solve the puzzles and escape.";
     public static final String UNUSABLE = "This item has no use anymore.";
     public static final String UNDISCOVERED = "No use for this yet.";
     public static final String LOCKED = "This item is locked.";
